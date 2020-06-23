e230c98ddc696db6ccac0fd620d01799e06c9fe6
babalenong <jotafauzanh@gmail.com>
2020-06-23 17:58:59 +0700

    how to csv?

--- detik/detik/spiders/detik_spider.py
+++ detik/detik/spiders/detik_spider.py
@@ -1,11 +1,21 @@
+# run "scrapy crawl detik -o detik.jl" on terminal
+
 import scrapy
+from scrapy.spiders import CSVFeedSpider
 
 class DetikSpider(scrapy.Spider):
     name = "detik"
-    # satu satu karena mager implementasi utk auto browse dan 
-    # ambil yg baru, tapi bisa kok pake scrapy 🙏
+    custom_settings = {
+        'FEED_URI': 'detik.csv',
+        'FEED_FORMAT': 'csv',
+        'FEED_EXPORT_FIELDS': ['judul', 'berita']
+}
+    
+    # satu satu karena mager implementasi utk di auto in 🙏
     start_urls = [
-        "https://finance.detik.com/berita-ekonomi-bisnis/5064934/ada-42-juta-pns-di-ri-kebanyakan-cuma-jadi-admin"
+        "https://finance.detik.com/berita-ekonomi-bisnis/5064934/ada-42-juta-pns-di-ri-kebanyakan-cuma-jadi-admin",
+        "https://finance.detik.com/berita-ekonomi-bisnis/d-5065025/bkpm-minta-tambahan-anggaran-rp-509-m-di-2021-ini-alokasinya",
+        "https://finance.detik.com/energi/d-5064740/anggaran-dipangkas-pembangunan-jargas-berkurang-jadi-127-ribu",
     ]
 
     def parse(self, response):
