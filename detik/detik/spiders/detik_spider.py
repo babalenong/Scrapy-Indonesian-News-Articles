57d33a2d3ec5f576b193d965b9fe1ae73fd76892
babalenong <jotafauzanh@gmail.com>
2020-06-30 16:12:12 +0700

    add artikel detik, kompas

--- detik/detik/spiders/detik_spider.py
+++ detik/detik/spiders/detik_spider.py
@@ -13,6 +13,28 @@ class DetikSpider(scrapy.Spider):
         "https://finance.detik.com/berita-ekonomi-bisnis/5064934/ada-42-juta-pns-di-ri-kebanyakan-cuma-jadi-admin",
         "https://finance.detik.com/berita-ekonomi-bisnis/d-5065025/bkpm-minta-tambahan-anggaran-rp-509-m-di-2021-ini-alokasinya",
         "https://finance.detik.com/energi/d-5064740/anggaran-dipangkas-pembangunan-jargas-berkurang-jadi-127-ribu",
+        "https://finance.detik.com/berita-ekonomi-bisnis/d-5073805/pendapatan-kai-anjlok-dari-rp-23-m-tinggal-rp-300-jutahari",
+        "https://finance.detik.com/bursa-dan-valas/d-5073851/kuartal-i-2020-pendapatan-lippo-karawaci-tembus-rp-31-t",
+        "https://finance.detik.com/moneter/d-5073479/dapat-rp-30-t-bank-bumn-harus-genjot-kredit-3-kali-lipat?single=1",
+        "https://finance.detik.com/energi/d-5072401/total-utang-pemerintah-ke-pertamina-tembus-rp-96-triliun",
+        "https://finance.detik.com/bursa-dan-valas/d-5072182/rupiah-menguat-tipis-dolar-as-turun-ke-14209",
+        "https://finance.detik.com/bursa-dan-valas/d-5069331/waskita-terima-pembayaran-tol-japek-layang-dan-lrt-palembang-rp-6-t",
+        "https://finance.detik.com/infrastruktur/d-5066707/basuki-minta-rp-280-m-buat-tangani-lumpur-lapindo",
+        "https://finance.detik.com/infrastruktur/d-5066584/bangun-tol-trans-sumatera-771-km-hutama-karya-tarik-utang-rp-12-t?single=1",
+        "https://finance.detik.com/berita-ekonomi-bisnis/d-5066522/perusahaan-korsel-dan-singapura-bikin-lab-corona-di-bandara-kalimantan",
+        "https://finance.detik.com/moneter/d-5066510/titipkan-rp-30-t-ke-bank-bumn-sri-mulyani-beri-bunga-murah",
+        "https://finance.detik.com/berita-ekonomi-bisnis/d-5066485/kai-catat-kenaikan-volume-penumpang-ka-reguler-hingga-49",
+        "https://finance.detik.com/bursa-dan-valas/d-5066465/245-saham-hijau-ihsg-ditutup-perkasa-di-4964",
+        "https://finance.detik.com/berita-ekonomi-bisnis/d-5066424/tok-banggar-dpr-sepakat-ekonomi-ri-45-55-di-2021",
+        "https://finance.detik.com/berita-ekonomi-bisnis/d-5066351/perpajakan-ditargetkan-tumbuh-105-di-2021-bagaimana-caranya",
+        "https://finance.detik.com/bumn/d-5066282/di-tengah-gelombang-phk-bumn-ini-rekrut-1490-karyawan-baru",
+        "https://finance.detik.com/berita-ekonomi-bisnis/d-5066241/dapat-suntikan-rp-11-t-laba-hutama-karya-diprediksi-turun-50",
+        "https://finance.detik.com/infrastruktur/d-5066180/basuki-minta-rp-112-t-buat-kebut-proyek-stadion-piala-dunia-u-20",
+        "https://finance.detik.com/berita-ekonomi-bisnis/d-5066179/pertama-kali-penerimaan-perpajakan-ri-minus-92",
+        "https://finance.detik.com/berita-ekonomi-bisnis/d-5066155/tenaga-medis-gugur-tangani-corona-dapat-santunan-hingga-rp-341-juta",
+        "https://finance.detik.com/infrastruktur/d-5066151/hk-masih-butuh-rp-51-t-rampungkan-771-km-tol-trans-sumatera",
+        "https://finance.detik.com/bursa-dan-valas/d-5066114/melonjak-88-poin-ihsg-kembali-dekati-5000",
+        "https://finance.detik.com/bursa-dan-valas/d-5066012/umumkan-ios-14-saham-apple-diprediksi-melejit-10"
     ]
 
     def parse(self, response):
--- kompas/kompas/spiders/kompas_spider.py
+++ kompas/kompas/spiders/kompas_spider.py
@@ -11,7 +11,30 @@ class KompasSpider(scrapy.Spider):
     # jadi dipilih yang cocok
     start_urls = [
         "https://money.kompas.com/read/2020/06/29/142550626/tergerus-ponsel-bagaimana-produsen-kamera-digital-bertahan?page=all#page2",
-        "https://money.kompas.com/read/2020/06/29/151814026/selama-psbb-penjualan-pertamina-anjlok-hingga-50-persen"
+        "https://money.kompas.com/read/2020/06/29/151814026/selama-psbb-penjualan-pertamina-anjlok-hingga-50-persen",
+        "https://money.kompas.com/read/2020/06/30/151729226/sepanjang-2019-jumlah-investor-pasar-modal-indonesia-tembus-248-juta",
+        "https://money.kompas.com/read/2020/06/30/150255326/netflix-dkk-wajib-laporkan-pungutan-pajak-per-3-bulan?page=all#page2",
+        "https://money.kompas.com/read/2020/06/30/140900026/ada-pandemi-covid-19-produksi-batu-bara-diproyeksi-masih-capai-target",
+        "https://money.kompas.com/read/2020/06/30/104449626/lippo-karawaci-raup-pendapatan-rp-310-triliun-pada-kuartal-i-2020?page=all#page2",
+        "https://money.kompas.com/read/2020/06/30/102741726/produksi-pesawat-menciut-40-persen-airbus-bakal-phk-20000-karyawan?page=all#page2",
+        "https://money.kompas.com/read/2020/06/30/090300826/di-tengah-pandemi-amazon-gelontorkan-rp-7-1-triliun-untuk-bonus-karyawan",
+        "https://money.kompas.com/read/2020/06/30/084300926/jual-20-persen-sahamnya-perusahaan-kosmetik-kim-kardashian-kini-bernilai-rp-14",
+        "https://money.kompas.com/read/2020/06/29/190023026/bfi-finance-bakal-bagi-dividen-rp-180-miliar",
+        "https://money.kompas.com/read/2020/06/29/173800826/-alfamart-bikin-coworking-space-di-lingkungan-kampus",
+        "https://money.kompas.com/read/2020/06/29/163046026/sri-mulyani-serapan-anggaran-kesehatan-dalam-pen-sudah-468-persen",
+        "https://money.kompas.com/read/2020/06/29/161241126/subsidi-listrik-dan-solar-diusulkan-turun-tahun-depan?page=all#page2",
+        "https://money.kompas.com/read/2020/06/29/151814026/selama-psbb-penjualan-pertamina-anjlok-hingga-50-persen",
+        "https://money.kompas.com/read/2020/06/29/121830926/bank-bumn-pakai-dana-rp-30-triliun-dari-pemerintah-untuk-salurkan-kredit?page=all#page2",
+        "https://money.kompas.com/read/2020/06/29/063956526/pria-40-tahun-salip-jack-ma-jadi-orang-terkaya-nomor-2-china-ini-penyebabnya?page=all#page2",
+        "https://money.kompas.com/read/2020/06/28/145035426/imbas-virus-corona-nike-rugi-rp-112-triliun",
+        "https://money.kompas.com/read/2020/06/28/141217926/arus-modal-asing-masuk-ke-sbn-capai-rp-58-triliun",
+        "https://money.kompas.com/read/2020/06/28/125400326/etf-indonesia-tertinggi-di-asean?page=all#page2",
+        "https://money.kompas.com/read/2020/06/28/091800926/gara-gara-coca-cola-kekayaan-mark-zuckerberg-lenyap-rp-102-6-triliun?page=all#page2",
+        "https://money.kompas.com/read/2020/06/28/083459526/erick-thohir-larang-bumn-ambil-proyek-kecil-yang-jadi-jatah-umkm?page=all#page2",
+        "https://money.kompas.com/read/2020/06/28/070800426/ada-pandemi-industri-tambak-udang-dan-es-batangan-di-bangka-tetap-tumbuh",
+        "https://money.kompas.com/read/2020/06/27/200000826/kemenkeu-prediksi-pertumbuhan-ekonomi-kuartal-ii-2020-negatif-3-8-persen",
+        "https://money.kompas.com/read/2020/06/27/180600926/di-jakarta-baru-0-04-persen-angkutan-umum-gunakan-kendaraan-listrik",
+        "https://money.kompas.com/read/2020/06/27/173700726/sri-mulyani-covid-19-timbulkan-efek-domino-yang-luar-biasa"
     ]
 
     def parse(self, response):
