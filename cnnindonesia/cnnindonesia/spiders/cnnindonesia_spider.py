75c5ed018ed8de439d084d185df3aee22d952679
babalenong <jotafauzanh@gmail.com>
2020-07-01 20:52:44 +0700

    fix merdeka for "multipages", add list url merdeka & cnnindonesia

--- cnnindonesia/cnnindonesia/spiders/cnnindonesia_spider.py
+++ cnnindonesia/cnnindonesia/spiders/cnnindonesia_spider.py
@@ -10,7 +10,31 @@ class CNNIndonesiaSpider(scrapy.Spider):
     # dan karena fungsi utamanya mendapatkan dataset, 
     # jadi dipilih yang cocok
     start_urls = [
-        "https://www.cnnindonesia.com/ekonomi/20200624185815-78-517083/pnm-minta-suntikan-rp15-t-untuk-raih-target-66-juta-nasabah"
+        "https://www.cnnindonesia.com/ekonomi/20200624185815-78-517083/pnm-minta-suntikan-rp15-t-untuk-raih-target-66-juta-nasabah",
+        "https://www.cnnindonesia.com/ekonomi/20200701123109-78-519463/bank-bumn-incar-kredit-rp90-t-tripel-dari-celengan-negara",
+        "https://www.cnnindonesia.com/ekonomi/20200701082954-78-519345/iuran-bpjs-kesehatan-naik-49-ribu-peserta-turun-kelas",
+        "https://www.cnnindonesia.com/ekonomi/20200701092409-78-519366/rupiah-melorot-ke-rp14270-jelang-rilis-inflasi-siang-ini",
+        "https://www.cnnindonesia.com/ekonomi/20200701162043-78-519622/rupiah-keok-ke-rp14282-per-dolar-as-pada-rabu-sore",
+        "https://www.cnnindonesia.com/ekonomi/20200701152630-78-519592/ramai-nasabah-bank-bukopin-batasi-antrean-150-orang-per-hari",
+        "https://www.cnnindonesia.com/ekonomi/20200618080425-78-514582/rerata-harga-pasar-koin-rp1000-sawit-viral-cuma-rp2000",
+        "https://www.cnnindonesia.com/ekonomi/20200617200959-78-514495/koin-rp1000-bergambar-kelapa-sawit-dijual-online-rp100-juta",
+        "https://www.cnnindonesia.com/ekonomi/20200616184714-78-514009/bri-akan-utang-rp14-t-dengan-bunga-19-persen-dari-bank-asing",
+        "https://www.cnnindonesia.com/ekonomi/20200615154010-83-513500/operasi-usus-buntu-yang-dijalani-yogi-lancar-berkat-jkn-kis",
+        "https://www.cnnindonesia.com/ekonomi/20200613152751-78-512973/akses-umkm-terbatas-kur-belum-terserap-capai-rp129-t",
+        "https://www.cnnindonesia.com/ekonomi/20200611160813-78-512311/pandemi-bikin-masyarakat-makin-melek-keuangan",
+        "https://www.cnnindonesia.com/ekonomi/20200612073157-78-512480/bank-artos-ganti-nama-jadi-bank-jago",
+        "https://www.cnnindonesia.com/ekonomi/20200611195334-78-512418/bpjs-kesehatan-tanggung-utang-rp65-t-ke-rumah-sakit",
+        "https://www.cnnindonesia.com/ekonomi/20200611170057-78-512345/iuran-naik-defisit-bpjs-kesehatan-tinggal-rp185-m-pada-2020",
+        "https://www.cnnindonesia.com/ekonomi/20200611172436-78-512353/menko-pmk-sebut-iuran-bpjs-harusnya-di-atas-rp200-ribu",
+        "https://www.cnnindonesia.com/ekonomi/20200529205105-78-508122/new-normal-mandiri-pangkas-kantor-cabang-dengan-realokasi",
+        "https://www.cnnindonesia.com/ekonomi/20200529145231-78-507958/bi-akan-tambah-bunga-simpanan-pemerintah-di-bank-sentral",
+        "https://www.cnnindonesia.com/ekonomi/20200528201605-78-507773/dapat-setoran-modal-rp255-m-bni-syariah-naik-jadi-buku-iii",
+        "https://www.cnnindonesia.com/ekonomi/20200528141055-78-507638/bank-korea-selatan-pangkas-bunga-acuan-jadi-05-persen",
+        "https://www.cnnindonesia.com/ekonomi/20200528144615-78-507639/sarankan-jual-reksa-dana-sinarmas-dirut-bibit-dicopot",
+        "https://www.cnnindonesia.com/ekonomi/20200608164322-78-511126/laba-bank-mandiri-tumbuh-melambat-ke-rp792-t-kuartal-i-2020",
+        "https://www.cnnindonesia.com/ekonomi/20200528092007-78-507517/harga-emas-antam-turun-ke-rp908-ribu-per-gram",
+        "https://www.cnnindonesia.com/ekonomi/20200527180659-78-507408/tumbuh-satu-digit-laba-bca-rp66-t-pada-kuartal-i-2020",
+        "https://www.cnnindonesia.com/ekonomi/20200527071106-78-507179/ojk-bekukan-sementara-7-reksa-dana-sinarmas-asset-management"
     ]
 
     def parse(self, response):
--- merdeka/merdeka/spiders/merdeka_spider.py
+++ merdeka/merdeka/spiders/merdeka_spider.py
@@ -10,13 +10,36 @@ class MerdekaSpider(scrapy.Spider):
     # dan karena fungsi utamanya mendapatkan dataset, 
     # jadi dipilih yang cocok
     start_urls = [
-      "https://www.merdeka.com/uang/umkm-sumbang-61-persen-ke-pdb-tapi-nilai-ekspor-umkm-masih-rendah.html",
-      "https://www.merdeka.com/uang/bantu-penanganan-corona-aice-group-sebar-apd-untuk-13-rumah-sakit-rujukan.html"
+        "https://www.merdeka.com/uang/umkm-sumbang-61-persen-ke-pdb-tapi-nilai-ekspor-umkm-masih-rendah.html",
+        "https://www.merdeka.com/uang/bantu-penanganan-corona-aice-group-sebar-apd-untuk-13-rumah-sakit-rujukan.html",
+        "https://www.merdeka.com/uang/mei-2020-kunjungan-turis-ke-indonesia-hanya-163000-orang.html",
+        "https://www.merdeka.com/uang/pemerintah-tetap-cairkan-rp-429-m-renovasi-rumah-di-5-kspn-prioritas-untuk-homestay.html",
+        "https://www.merdeka.com/uang/pertumbuhan-ekonomi-dunia-diprediksi-negatif-di-kuartal-ii-2020-termasuk-ri.html",
+        "https://www.merdeka.com/uang/dpr-setuju-pemerintah-cicil-utang-ke-pupuk-indonesia-rp57-triliun-tahun-ini.html",
+        "https://www.merdeka.com/uang/bos-ojk-harap-penempatan-dana-rp30-triliun-ke-bank-himbara-bantu-pulihkan-ekonomi.html",
+        "https://www.merdeka.com/uang/tak-dilunasi-sejak-2017-utang-pemerintah-ke-pupuk-indonesia-bengkak-jadi-rp171-t.html",
+        "https://www.merdeka.com/uang/dampak-corona-produksi-rokok-diprediksi-turun-hingga-23-persen.html",
+        "https://www.merdeka.com/uang/bank-indonesia-catat-kepemilikan-sbn-per-23-juni-rp-4475-triliun.html",
+        "https://www.merdeka.com/uang/indeks-kesiapan-digital-umkm-di-jabodetabek-masih-tahap-menengah-ini-penyebabnya.html",
+        "https://www.merdeka.com/uang/pemerintah-siapkan-umkm-masuk-pengadaan-barang-bumn-dengan-proyek-di-bawah-rp14-m.html",
+        "https://www.merdeka.com/uang/rupiah-melemah-ke-rp14220usd-dipicu-kekhawatiran-meningkatnya-penyebaran-covid-19.html",
+        "https://www.merdeka.com/uang/lman-danai-pembebasan-lahan-proyek-psn-senilai-rp533-triliun.html",
+        "https://www.merdeka.com/uang/bank-indonesia-ramal-inflasi-juni-2020-sebesar-minus-001-persen.html",
+        "https://www.merdeka.com/uang/data-kadin-catat-64-juta-orang-di-phk-dan-dirumahkan-hingga-mei-2020.html",
+        "https://www.merdeka.com/uang/pengunjung-mal-hanya-40-persen-pengelola-minta-pemerintah-bantu-soal-kelistrikan.html",
+        "https://www.merdeka.com/uang/data-kadin-catat-64-juta-orang-di-phk-dan-dirumahkan-hingga-mei-2020.html",
+        "https://www.merdeka.com/uang/nilai-tukar-rupiah-melemah-dipicu-kekhawatiran-potensi-gelombang-kedua-covid-19.html",
+        "https://www.merdeka.com/uang/jalankan-program-rehabilitasi-hutan-klhk-minta-tambahan-anggaran-rp53-triliun.html",
+        "https://www.merdeka.com/uang/per-24-juni-blt-dana-desa-sudah-tersalur-ke-69424-desa-dengan-nilai-rp43-triliun.html",
+        "https://www.merdeka.com/uang/360818-wajib-pajak-telah-disetujui-permohonan-insentif-pajaknya.html",
+        "https://www.merdeka.com/uang/jasindo-salurkan-dana-kemitraan-rp2446-miliar-untuk-umkm-di-2020.html",
+        "https://www.merdeka.com/uang/imf-kembali-pangkas-proyeksi-pertumbuhan-ekonomi-global-jadi-minus-49-persen.html",
+        "https://www.merdeka.com/uang/tingkatkan-daya-saing-global-ikm-gula-palma-terapkan-teknologi-industri-40.html"
     ]
 
     def parse(self, response):
         for content in response.css("div.mdk-body-detail"):
             yield {
                 'judul': content.css("div.mdk-dt-headline h1::text").get(),
-                'berita': content.css("div.mdk-body-paragraph p::text").getall()
+                'berita': content.css("div.mdk-body-detail p::text").getall()
             }
